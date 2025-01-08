0f05c2342d695ed19df9154cb35d3ff68052a49e
Muhammad Anas <anasabid607@gmail.com>
2025-01-08 18:35:41 +0100

    java

---
+++ Muhammad_Anas_Funtion_1/src/muhammad_anas_funtion_1/Muhammad_Anas_Funtion_1.java
@@ -0,0 +1,13 @@
+
+package muhammad_anas_funtion_1;
+
+public class Muhammad_Anas_Funtion_1 {
+public static float quadratic(float number) {
+        return number * number;
+}
+    public static void main(String[] args) {
+        float result = quadratic(5);
+        System.out.printf("El quadrat de 5 és %.2f%n", result);        
+    }
+    
+}
