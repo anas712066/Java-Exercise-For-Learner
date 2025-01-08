bc99a119df0e92e9628d94166f262dd70a153e1a
Muhammad Anas <anasabid607@gmail.com>
2025-01-08 18:55:17 +0100

    java

---
+++ Muhammad_Anas_Function_2/src/muhammad_anas_function_2/Muhammad_Anas_Function_2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package muhammad_anas_function_2;
+import java.util.Scanner;
+public class Muhammad_Anas_Function_2 {
+public static boolean mayor(float num1, float num2) {
+        boolean mayor = false;
+        if (num1 > num2) {
+            mayor = true;
+        }
+        return mayor;
+}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Introdueix el primer número: ");
+        float num1 = input.nextFloat();
+        System.out.print("Introdueix el segon número: ");
+        float num2 = input.nextFloat();
+        boolean result = mayor(num1, num2);
+        if (result = false) {
+            
+        }
+        
+    }
+    
+}
