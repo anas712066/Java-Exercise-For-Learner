755127d031a1730fee6c1da7ce64fdaba3b7fd53
Muhammad Anas <anasabid607@gmail.com>
2025-01-13 20:47:41 +0100

    java

--- Muhammad_Anas_funcion_5/src/muhammad_anas_funcion_5/Muhammad_Anas_funcion_5.java
+++ Muhammad_Anas_funcion_5/src/muhammad_anas_funcion_5/Muhammad_Anas_funcion_5.java
@@ -4,17 +4,29 @@
  */
 package muhammad_anas_funcion_5;
 
-/**
- *
- * @author Argo
- */
 public class Muhammad_Anas_funcion_5 {
-
-    /**
-     * @param args the command line arguments
-     */
+    public static int diesMes(int més, int any) {
+        int diesMes = 0;
+        if (més == 1 || més == 3 || més == 5 || més == 7 || més == 8 || més == 10 || més ==12) {
+            diesMes = 31;
+        } else {
+            if (més == 4 || més == 6 || més == 9 || més == 11) {
+                diesMes = 30;
+            } else {
+                if (més == 2) {
+                    if (any % 4 == 0 && any % 100 != 0 || any % 400 == 0) {
+                        diesMes = 29;
+                    } else {
+                        diesMes = 28;
+                    }
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
+
+Implementa una funció que rebi com a arguments dos enters que representen un mes i un any, i torni com a resultat el nombre de dies d'aquest mes. Si és febrer s'ha de comprovar si és un any de traspàs
